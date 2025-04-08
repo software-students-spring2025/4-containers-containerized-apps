65da5aa73a2390054b1a0110bd4066532998548a
HuixinLi-Elena <hl4909@nyu.edu>
2025-04-08 17:42:10 -0400

    Upload docker-compose.yml and revised main.py of ml client

--- machine-learning-client/main.py
+++ machine-learning-client/main.py
@@ -8,6 +8,7 @@ import numpy
 import tensorflow as tf
 import cv2  # pylint: disable=import-error
 import pymongo
+import os
 
 print("Python version:", sys.version)
 print("NumPy version:", numpy.__version__)
@@ -17,14 +18,14 @@ print("OpenCV version:", cv2.__version__)  # pylint: disable=all
 print("PyMongo version:", pymongo.__version__)
 print("All libraries imported successfully!")
 
-# Try to connect to MongoDB (will fail but shows the code works)
+# Try to connect to MongoDB
 try:
-    client = pymongo.MongoClient("mongodb://mongodb:27017/")
+    MONGO_URI = os.environ.get("MONGO_URI", "mongodb://mongodb:27017/")
+    client = pymongo.MongoClient(MONGO_URI)
     print("MongoDB connection attempt made")
-except pymongo.errors.ConnectionFailure as e:
-    print("Expected MongoDB connection error:", e)
-except Exception as e:  # pylint: disable=broad-except
-
-    print("Unexpected error when connecting to MongoDB:", e)
+    db_list = client.list_database_names()
+    print("Database connection successful, found databases:", db_list)
+except Exception as e:
+    print("Error connecting to MongoDB:", e)
 
 print("ML client test completed successfully!")
